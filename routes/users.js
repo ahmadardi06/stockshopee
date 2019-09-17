5c8e45fc1396e302c9f0dfa2bd37e9c6dd88546d
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-09-18 06:59:46 +0700

    github chat

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+// var TOKEN_BOT = "845430643:AAGDRv1fRcQaEhNKLHLfPY_Ow2qPaSflbjE";
+var TOKEN_BOT = "915963347:AAE7TRrFASw5yuV0wEzfeeX6ng-RwJdeP0o";
+// chat_id client faruq
+var CHAT_ID = 812449714;
+var TelegramBotClient = require('telegram-bot-client');
+var client = new TelegramBotClient(TOKEN_BOT);
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/send', function(req, res, next) {
+	if(!req.query.chat_id) return res.json({msg: 'query chat_id required.'}); 
+	if(!req.query.message) return res.json({msg: 'query message required.'});
+
+	if(req.query.chat_id && req.query.message){
+  	client.sendMessage(req.query.chat_id, req.query.message);
+	} else {
+  	client.sendMessage(CHAT_ID, req.query.message);
+	}
 });
 
 module.exports = router;
